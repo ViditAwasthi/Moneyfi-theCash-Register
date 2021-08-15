78ac57bc9e9c9881802413dceabc7bde04eba438
Vidit Awasthi <viditawasthi2@gmail.com>
2021-08-16 00:11:05 +0530

    minor fixes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const noOfNotes = document.querySelectorAll(".no-of-notes");
 const nextButton = document.querySelector("#next-button");
 const hiddenContainer = document.querySelector("#hidden-container");
 const cashErrorMessage = document.querySelector("#cash-error-message");
+const amountReturnMessage = document.querySelector("#amount-return-message");
 const returnTable = document.querySelector("#return-table");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
@@ -29,26 +30,32 @@ checkButton.addEventListener("click", function validateAmount() {
     errorMessage.innerHTML = "";
     cashErrorMessage.innerHTML = "";
     returnTable.style.display = "none";
+    amountReturnMessage.innerText ="";
 
     if (isNaN(cashAmount.value) || isNaN(billAmount.value)) {
         cashErrorMessage.innerHTML = "Cash & Bill Amount should be a number!"
         returnTable.style.display = "none";
+        amountReturnMessage.innerText ="";
         return;
     } else if (cashAmount.value <= 0 || billAmount.value <= 0) {
         cashErrorMessage.innerHTML = "Please Input a Valid Cash & Bill Amount!"
         returnTable.style.display = "none";
+        amountReturnMessage.innerText ="";
         return;
     } else if (Number(cashAmount.value) === Number(billAmount.value)) {
         cashErrorMessage.innerHTML = "No Change Required! Full Amount Paid"
         returnTable.style.display = "none";
+        amountReturnMessage.innerText ="";
         return;
     } else {
         if (Number(cashAmount.value) < Number(billAmount.value)) {
             returnTable.style.display = "none";
+            amountReturnMessage.innerText ="";
             cashErrorMessage.innerHTML = "Cash Amount Should be Greater than the Bill Amount"
             return;
         } else {
             const cashToBeReturned = cashAmount.value - billAmount.value;
+            amountReturnMessage.innerText ="Total amount to be returned is: "+cashToBeReturned;
             calculateChange(cashToBeReturned);
         }
     }
@@ -59,6 +66,7 @@ checkButton.addEventListener("click", function validateAmount() {
 function calculateChange(amountToBeReturned) {
     if (Number(cashAmount.value) < Number(billAmount.value)) {
         returnTable.style.display = "none";
+        amountReturnMessage.innerText ="";
         cashErrorMessage.innerHTML = "Cash Amount Should be Greater than the Bill Amount"
         return;
     } else {
